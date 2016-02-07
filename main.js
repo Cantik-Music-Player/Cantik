31ba98a693461c5433c5291bda39d755a36cd457
Cyprien G <cyprien.guillemot@gmail.com>
2016-02-07 16:45:03 +0100

    ENH: Integrate design in an Electron app

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ require('crash-reporter').start();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
+var window = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -21,19 +21,18 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  window = new BrowserWindow({title: 'Cantik', icon: 'static/images/icon.png'});
+  window.maximize();
+  window.setMenu(null);
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-  // Open the DevTools.
-  mainWindow.openDevTools();
+  window.loadUrl('file://' + __dirname + '/static/index.html');
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  window.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
+    window = null;
   });
 });
