8d302bf58d954f69cc62ae39bc304d9ce7924c5a
Cyprien G <cyprien.guillemot@gmail.com>
2016-09-25 14:21:45 +0200

    CLN: Clean repo

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in plugins/*; do
-    cd $i && npm run clean
-    cd ../..
-done
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in plugins/*; do
-    cd $i && npm test || exit 1
-    cd ../..
-done
-
-cd src && mocha --timeout 200000 --compilers coffee:coffee-script/register test && cd ..
