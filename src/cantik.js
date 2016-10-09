dec553692d1001429fa4548bbc4dede3c86d21aa
Cyprien G <cyprien.guillemot@gmail.com>
2016-10-09 12:40:53 +0200

    ENH: Add settings to default packages

--- src/cantik.js
+++ src/cantik.js
@@ -1,5 +1,7 @@
 require('coffee-script/register');
+var electron = require('electron')
 var PluginManager = require('./plugins-manager');
 
 exports.pluginManager = new PluginManager();
-exports.utils = require('./utils.coffee')
+exports.utils = require('./utils.coffee');
+exports.app = electron.remote.app;
